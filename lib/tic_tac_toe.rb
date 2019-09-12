f23f384cb2838700eb0fc3e67c414b9285478294
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-12 15:46:13 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,116 @@
+class TicTacToe
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+  
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+  
+  def won?
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" &&       position_3 == "O"
+      return win_combo
+      end
+   }
+    return false
+  end
+
+  def full?
+    @board.all?{|full| full == "X" || full == "O"}
+  end
+  
+  def draw?
+    if full? && !won? 
+      true
+    end
+  end
+
+  def over?
+    if full? && won?
+     true
+    elsif draw?
+     true
+    elsif won? && !full?
+     true
+    end
+  end
+
+  def winner
+    index = won?
+    if index == false
+      return nil
+    else
+    if @board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+   end
+  end
+
+  def play
+    until over? == true
+     turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    else draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
